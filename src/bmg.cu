15fb162a4004950df91fcf8166e28fd7649701c1
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-06 13:19:52 +0100

    Added GPU code

--- src/bmg.cu
+++ src/bmg.cu
@@ -19,6 +19,54 @@
 
 using namespace BMG;
 
+size_t std::min(int a, size_t b)
+{
+    if (a < 0)
+        return 0;
+    return (a < b) ? a : b;
+}
+__global__ void generate_BM_kernel(double** BM, std::size_t n, double start, int offset)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    curandState state;
+
+    curand_init(clock64(), tid + offset, 0, &state);
+    
+    BM[threadIdx.x + offset][0] = start;
+
+    for (size_t i = 1; i < n; i++)
+    {
+        BM[threadIdx.x + offset][i] = BM[threadIdx.x + offset][i - 1] + curand_normal_double(&state);
+    }
+}
+
+void generate_default_BMs_on_cuda(std::size_t n, std::vector<std::vector<double>>& BM, std::size_t N, double start)
+{
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+
+    int nb_max_thread = prop.maxThreadsPerBlock;
+
+    double** d_BM;
+    cudaMalloc((void**)&d_BM, N * sizeof(double*));
+    for (size_t i = 0; i < N; i++)
+    {
+        BM[i].resize(n);
+        cudaMalloc((void**)&d_BM[i], n * sizeof(double));
+    }
+
+    for (int i = 0; i < N / nb_max_thread + 1; i++)
+        generate_BM_kernel<<<1, std::min(nb_max_thread, N)>>>(d_BM, n, start, i * nb_max_thread);
+
+    for (size_t i = 0; i < N; i++)
+    {
+        cudaMemcpy(BM[i].data(), d_BM[i], n * sizeof(double), cudaMemcpyDeviceToHost);
+        cudaFree(d_BM[i]);
+    }
+    cudaFree(d_BM);
+    d_BM = nullptr;
+}
+
 void BMG::generate_default_BM(std::size_t n, std::vector<double>& BM, double start)
 {
     BM.resize(n);
@@ -39,10 +87,10 @@ void BMG::generate_default_BM(std::size_t n, std::vector<double>& BM, double sta
 void BMG::generate_default_BM(std::size_t n, std::vector<std::vector<double>>& BM, std::size_t N, double start, bool cuda)
 {
     BM.resize(N);
-
-    if (cuda && N > std::thread::hardware_concurrency())
+    int nb_GPU(0);
+    if (cuda && N > std::thread::hardware_concurrency() && cudaGetDeviceCount(&nb_GPU) == cudaSuccess && nb_GPU > 0)
     {
-        
+        generate_default_BMs_on_cuda(n, BM, N, start);
     }
     else
     {
@@ -56,7 +104,7 @@ void BMG::generate_default_BM(std::size_t n, std::vector<std::vector<double>>& B
         std::thread *threads = new std::thread[N];
         for (size_t i = 0; i < N; i++)
         {
-            threads[i] = std::thread([n, &BM, start, i]() {
+            threads[i] = std::thread([n, &BM, start, i](void) -> void {
                 BMG::generate_default_BM(n, BM[i], start);
             });
         }
