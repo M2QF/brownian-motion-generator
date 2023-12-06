2151085b84eaea89f285fec7032a2b9816b22bec
Thomas Roiseux <thomas.roiseux@outlook.com>
2023-12-06 19:56:30 +0100

    Added tests

--- tests/test.cu
+++ tests/test.cu
@@ -20,6 +20,58 @@
 
 using namespace std;
 
+#define SIZE 1000
+#define TOLERANCE 1.0 / SIZE * 100
+
+/**
+ * @brief Test the size of the vector
+ * 
+ */
+void check_size()
+{
+    cout << "Testing the size of the vector" << endl;
+    std::vector<double> BM;
+
+    BMG::generate_default_BM(SIZE, BM);
+
+    CU_ASSERT_EQUAL(BM.size(), SIZE);
+}
+
+void isNormalIncrement()
+{
+    cout << "Testing the normality of the increment" << endl;
+    std::vector<double> BM;
+
+    BMG::generate_default_BM(SIZE, BM);
+
+    std::vector<double> increments;
+    for (int i = 1; i < SIZE; i++)
+    {
+        increments.push_back(BM[i] - BM[i - 1]);
+    }
+
+    double mean = 0;
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        mean += increments[i];
+    }
+    mean /= SIZE - 1;
+
+    double variance = 0;
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        variance += (increments[i] - mean) * (increments[i] - mean);
+    }
+
+    variance /= SIZE - 1;
+
+    cout << "Mean : " << mean << endl;
+    cout << "Variance : " << variance << endl;
+
+    CU_ASSERT_DOUBLE_EQUAL(mean, 0, TOLERANCE);
+    CU_ASSERT_DOUBLE_EQUAL(variance, 1, TOLERANCE);
+}
+
 /**
  * @brief Test the CUDA implementation of the algorithm
  * 
@@ -29,7 +81,7 @@ using namespace std;
  */
 int main(int argc, char **argv)
 {
-    CU_initialize_registry();
+    
     bool GPU = true;
     for (int i = 0; i < argc; i++)
     {
@@ -41,11 +93,14 @@ int main(int argc, char **argv)
         }
     }
 
-    std::vector<double> BM;
+    CU_initialize_registry();
 
-    BMG::generate_default_BM(1000, BM);
+    CU_pSuite pSuite = CU_add_suite("Default Brownian motion", 0, 0);
+    CU_ADD_TEST(pSuite, check_size);
+    CU_ADD_TEST(pSuite, isNormalIncrement);
 
-    CU_ASSERT_EQUAL(BM.size(), 1000);
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
 
     CU_cleanup_registry();
     return 0;
